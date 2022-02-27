63bf29c5385978fae2519b6a596fb76fb1d28368
Jo <88668777+jonars99@users.noreply.github.com>
2022-02-27 18:42:42 +0000

    basic component set up

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import Main from './components/main';
+import Projects from './components/projects';
+import About from './components/about';
+import Contact from './components/contact';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Main />
+      <Projects />
+      <About />
+      <Contact />
     </div>
   );
 }
---
+++ react-portfolio/src/components/projects.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Projects () {
+  return (
+    <div>
+      <h4>projects</h4>
+    </div>
+  )
+}
+
+export default Projects;
