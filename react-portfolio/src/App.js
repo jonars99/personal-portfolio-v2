dd6033f731f9b4b806e0d9f19f815ce13e90e83a
Jo <88668777+jonars99@users.noreply.github.com>
2022-03-01 14:08:20 +0000

    set up basic landing page

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,17 +1,20 @@
-import Main from './components/main';
-import Projects from './components/projects';
-import About from './components/about';
-import Contact from './components/contact';
-import './App.css';
+import React from 'react';
+import Main from './components/Main';
+import Projects from './components/Projects';
+import About from './components/About';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+import './App.scss';
 
 function App() {
   return (
-    <div>
+    <React.Fragment>
       <Main />
+      <Navbar />
       <Projects />
       <About />
       <Contact />
-    </div>
+    </React.Fragment>
   );
 }
 
---
+++ react-portfolio/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Navbar () {
+  return (
+    <h2>nav</h2>
+  )
+}
+
+export default Navbar;
