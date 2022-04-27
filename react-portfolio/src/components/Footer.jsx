aea9406ee9780da8955c82e30c42ae38f82d9adf
Jo <88668777+jonars99@users.noreply.github.com>
2022-04-27 14:35:48 +0100

    hide p on xs screens and add link

--- react-portfolio/src/components/Footer.jsx
+++ react-portfolio/src/components/Footer.jsx
@@ -4,8 +4,8 @@ function Footer () {
   return (
     <footer id="footer" className="d-flex justify-content-evenly align-items-center">
       <p className="copyright px-3 py-2 m-0">&copy; 2022 Joanna Redihough</p>
-      <p className="m-0">designed and built by me</p>
-      <p className="m-0">icons by svgrepo.com</p>
+      <p className="d-none d-sm-flex m-0">designed and built by me</p>
+      <p className="m-0">icons by <a href="https://www.svgrepo.com" target="_blank">svgrepo.com</a></p>
     </footer>
   )
 }
