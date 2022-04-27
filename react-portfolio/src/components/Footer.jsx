804ef4cae14c5045a774e502238a5259ac5a8bc9
Jo <88668777+jonars99@users.noreply.github.com>
2022-04-27 16:48:01 +0100

    add rel=noreferrer to anchor tags

--- react-portfolio/src/components/Footer.jsx
+++ react-portfolio/src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer () {
     <footer id="footer" className="d-flex justify-content-evenly align-items-center">
       <p className="copyright px-3 py-2 m-0">&copy; 2022 Joanna Redihough</p>
       <p className="d-none d-sm-flex m-0">designed and built by me</p>
-      <p className="m-0">icons by <a href="https://www.svgrepo.com" target="_blank">svgrepo.com</a></p>
+      <p className="m-0">icons by <a href="https://www.svgrepo.com" target="_blank" rel="noreferrer noopener">svgrepo.com</a></p>
     </footer>
   )
 }
